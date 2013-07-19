b4ae2c64ce9c31e6629a7d8dc5ebbd26d0c84f96
mqprichard <mqprichard@gmail>
2013-07-19 12:19:10 -0700

    Added missing res files and refactored to fit standard Android gradle build structure: added src/instrumentTest source set

---
+++ src/instrumentTest/java/com/cloudbees/gasp/model/ReviewTest.java
@@ -0,0 +1,15 @@
+package com.cloudbees.gasp.model;
+
+import junit.framework.TestCase;
+
+// Dummy test
+public class ReviewTest extends TestCase {
+    protected void setUp() {}
+    protected void tearDown() {}
+
+    public void testShouldPass() {
+        Review review = new Review();
+        assert(true);
+    }
+}
+
