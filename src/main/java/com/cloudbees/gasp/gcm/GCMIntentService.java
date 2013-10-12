e858a3e6ef2e0eae3fdfd74fb237faa3a620da09
mqprichard <mqprichard@gmail>
2013-10-12 11:15:40 -0700

    Added FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_SINGLE_TOP to Intent in sendNotification()

--- src/main/java/com/cloudbees/gasp/gcm/GCMIntentService.java
+++ src/main/java/com/cloudbees/gasp/gcm/GCMIntentService.java
@@ -101,7 +101,8 @@ public class GCMIntentService extends IntentService {
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, MainActivity.class), 0);
+                new Intent(this, MainActivity.class).setFlags(
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
