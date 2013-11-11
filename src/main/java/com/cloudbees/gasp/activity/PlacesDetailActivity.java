c81bcf9839fce02071a7275ed368c64aded0703a
mqprichard <mqprichard@gmail>
2013-11-10 22:14:29 -0800

    Added GaspReviewFragment and GaspRestaurantFragment to PlacesDetailActivity

--- src/main/java/com/cloudbees/gasp/activity/PlacesDetailActivity.java
+++ src/main/java/com/cloudbees/gasp/activity/PlacesDetailActivity.java
@@ -3,7 +3,9 @@ package com.cloudbees.gasp.activity;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,11 +16,14 @@ import android.widget.TextView;
 
 import com.cloudbees.gasp.R;
 import com.cloudbees.gasp.fragment.GaspDatabaseFragment;
+import com.cloudbees.gasp.fragment.GaspRestaurantFragment;
+import com.cloudbees.gasp.fragment.GaspReviewFragment;
 import com.cloudbees.gasp.model.PlaceDetail;
 import com.cloudbees.gasp.model.PlaceEvent;
 import com.cloudbees.gasp.model.Restaurant;
 import com.cloudbees.gasp.model.Review;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,9 +56,14 @@ public class PlacesDetailActivity extends Activity {
     private ListView mEventsView;
 
     private String mPlacesId;
+    private PlaceDetail mPlaceDetail;
     private List<Review> mReviews;
     private boolean mGaspRestaurant = false;
     private GaspDatabaseFragment mGaspDatabaseFragment;
+    private GaspRestaurantFragment mGaspRestaurantFragment;
+    private GaspReviewFragment mGaspReviewFragment;
+    private static String SERVER_URL;
+    private Restaurant mRestaurant;
 
     private ArrayAdapter<String> mEventAdapter;
     private final ArrayList<String> mEventList = new ArrayList<String>();
@@ -85,7 +95,24 @@ public class PlacesDetailActivity extends Activity {
         reviewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Add Review");
+                try {
+                    SharedPreferences gaspSharedPreferences =
+                            PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    URL gaspUrl = new URL(gaspSharedPreferences.getString("gasp_reviews_uri", ""));
+
+                    Review review = new Review();
+                    review.setStar(5);
+                    review.setComment("Very good");
+                    review.setRestaurant_id(mRestaurant.getId());
+                    review.setUser_id(1);
+
+                    mGaspReviewFragment.addReview(review, gaspUrl);
+
+                    mGaspRestaurant = true;
+                    setButtons();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -95,7 +122,23 @@ public class PlacesDetailActivity extends Activity {
         restaurantButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Add Restaurant");
+                try {
+                    SharedPreferences gaspSharedPreferences =
+                            PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    URL gaspUrl = new URL(gaspSharedPreferences.getString("gasp_restaurants_uri", ""));
+
+                    Restaurant restaurant = new Restaurant();
+                    restaurant.setName(mPlaceDetail.getName());
+                    restaurant.setPlacesId(mPlaceDetail.getId());
+                    restaurant.setWebsite(mPlaceDetail.getWebsite());
+
+                    mGaspRestaurantFragment.addRestaurant(restaurant, gaspUrl);
+
+                    mGaspRestaurant = true;
+                    setButtons();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -116,7 +159,31 @@ public class PlacesDetailActivity extends Activity {
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         mGaspDatabaseFragment = new GaspDatabaseFragment();
+        mGaspRestaurantFragment = new GaspRestaurantFragment() {
+            @Override
+            public void onSuccess(String location) {
+                Log.d(TAG, "Gasp! restaurant added");
+            }
+
+            @Override
+            public void onFailure() {
+                Log.e(TAG, "Error adding Gasp! review");
+            }
+        };
+        mGaspReviewFragment = new GaspReviewFragment() {
+            @Override
+            public void onSuccess(String location) {
+                Log.d(TAG, "Gasp! review added");
+            }
+
+            @Override
+            public void onFailure() {
+                Log.e(TAG, "Eror adding Gasp! review");
+            }
+        };
         ft.add(mGaspDatabaseFragment, "Gasp Database Fragment");
+        ft.add(mGaspRestaurantFragment, "Gasp Restaurant Fragment");
+        ft.add(mGaspReviewFragment, "Gasp Review Fragment");
         ft.commit();
     }
 
@@ -150,6 +217,7 @@ public class PlacesDetailActivity extends Activity {
         if (restaurant != null) {
             Log.d(TAG, "Gasp Restaurant Id: " + restaurant.getId());
             mGaspRestaurant = true;
+            mRestaurant = restaurant;
             mReviews = mGaspDatabaseFragment.getReviewsByRestaurant(restaurant.getId());
             for (Review review : mReviews) {
                 Log.d(TAG, review.toString());
@@ -164,20 +232,20 @@ public class PlacesDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         try {
-            PlaceDetail place = (PlaceDetail) getIntent().getSerializableExtra("PlaceDetail");
-            mPlacesId = place.getId();
+            mPlaceDetail = (PlaceDetail) getIntent().getSerializableExtra("PlaceDetail");
+            mPlacesId = mPlaceDetail.getId();
 
-            Log.d(TAG, "Name: " + place.getName());
-            Log.d(TAG, "Website " + place.getWebsite());
-            Log.d(TAG, "Address: " + place.getFormatted_address());
-            Log.d(TAG, "Id: " + place.getId());
-            Log.d(TAG, "Lat: " + place.getGeometry().getLocation().getLat());
-            Log.d(TAG, "Lng: " + place.getGeometry().getLocation().getLng());
+            Log.d(TAG, "Name: " + mPlaceDetail.getName());
+            Log.d(TAG, "Website " + mPlaceDetail.getWebsite());
+            Log.d(TAG, "Address: " + mPlaceDetail.getFormatted_address());
+            Log.d(TAG, "Id: " + mPlaceDetail.getId());
+            Log.d(TAG, "Lat: " + mPlaceDetail.getGeometry().getLocation().getLat());
+            Log.d(TAG, "Lng: " + mPlaceDetail.getGeometry().getLocation().getLng());
 
             setViews();
             addFragments();
-            showLocationDetails(place);
-            showEventDetails(place);
+            showLocationDetails(mPlaceDetail);
+            showEventDetails(mPlaceDetail);
             addItemClickListener();
             addReviewButtonListener();
             addRestaurantButtonListener();
