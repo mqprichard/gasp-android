f0e981b95064fb465120d1391946f96db547b771
Mark Prichard <mqprichard@gmail.com>
2013-12-09 23:06:07 -0800

    Added OptionsMenu tests to ConsoleActivityTest Fixed ParentActivity bug in AndroidManifest

--- src/instrumentTest/java/com/cloudbees/gasp/activity/ConsoleActivityTest.java
+++ src/instrumentTest/java/com/cloudbees/gasp/activity/ConsoleActivityTest.java
@@ -1,6 +1,11 @@
 package com.cloudbees.gasp.activity;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
+
+import com.cloudbees.gasp.R;
 
 /**
  * Copyright (c) 2013 Mark Prichard, CloudBees
@@ -33,6 +38,36 @@ public class ConsoleActivityTest extends ActivityInstrumentationTestCase2<Consol
         mActivity = getActivity();
     }
 
-    public void testMainActivity() throws Throwable {
+    private void testOptionsMenuStartActivity(String className, int id){
+        Instrumentation.ActivityMonitor am = getInstrumentation().addMonitor(className, null, false);
+        getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+        getInstrumentation().invokeMenuActionSync(mActivity, id, 0);
+        Activity a = getInstrumentation().waitForMonitorWithTimeout(am, 1000);
+        assertEquals(true, getInstrumentation().checkMonitorHit(am, 1));
+        a.finish();
+    }
+
+    public void testOptionsMenuRestaurants() throws Throwable {
+        testOptionsMenuStartActivity(RestaurantListActivity.class.getName(), R.id.gasp_restaurants_data);
+    }
+
+    public void testOptionsMenuReviews() throws Exception {
+        testOptionsMenuStartActivity(ReviewListActivity.class.getName(), R.id.gasp_reviews_data);
+    }
+
+    public void testOptionsMenuUsers() throws Exception {
+        testOptionsMenuStartActivity(UserListActivity.class.getName(), R.id.gasp_users_data);
+    }
+
+    public void testOptionsMenuTwitter() throws Exception {
+        testOptionsMenuStartActivity(TwitterStreamActivity.class.getName(), R.id.gasp_menu_twitter);
+    }
+
+    public void testOptionsMenuPlaces() throws Exception {
+        testOptionsMenuStartActivity(PlacesActivity.class.getName(), R.id.gasp_menu_places);
+    }
+
+    public void testOptionsMenuPreferences() throws Exception {
+        testOptionsMenuStartActivity(SetPreferencesActivity.class.getName(), R.id.gasp_settings);
     }
 }
